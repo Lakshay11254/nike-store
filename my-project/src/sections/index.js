b4018631e4b3b9ddaac3dd6d48ad7d4695c8b92e
Lakshay <lakshay.sbp@gmail.com>
2023-09-24 13:59:34 +0530

    Moving onto the Navbar

---
+++ my-project/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './Subscribe';
+import Subscribe from './Subscribe';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Footer from './Footer';
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SpecialOffer,
+    SuperQuality,
+    CustomerReviews,
+    Footer,
+}
