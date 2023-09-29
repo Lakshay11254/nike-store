1b2c8729fee4c062093f9ee2bf39658ea6e70609
Lakshay <lakshay.sbp@gmail.com>
2023-09-29 17:51:13 +0530

    bug fixed

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,6 +1,6 @@
 import Nav from './component/Nav';
 import { Hero,
-  PopularProducts,
+  PopularProduct,
   Services,
   Subscribe,
   SpecialOffer,
@@ -16,7 +16,7 @@ const App = () => (
         <Hero />
       </section>
       <section className='padding'>
-        <PopularProducts />
+        <PopularProduct/>
       </section>
       <section className='padding'>
         <SuperQuality />
